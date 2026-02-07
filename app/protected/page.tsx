a5e389a29666f8c26f817625a5eaf3553f30a35d
Daniel David <dd3269@columbia.edu>
2026-02-07 13:53:48 -0500

    directing refactoring

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -13,7 +13,7 @@ export default async function ProtectedPage() {
     const { data: { user } } = await supabase.auth.getUser()
 
     if (!user) {
-        return redirect('/login')
+        return redirect('/')
     }
 
     return (
